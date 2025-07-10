0effd1850350bbfd8927ce59bf86372f9afb25d3
florus <florus@tackyon.it>
2025-07-10 09:18:39 +0200

    prettier fix

--- src/utils/flagsmith.ts
+++ src/utils/flagsmith.ts
@@ -1,8 +1,8 @@
-import Flagsmith from 'flagsmith-nodejs';
+import Flagsmith from "flagsmith-nodejs";
 
 const flagsmith = new Flagsmith({
-	environmentKey: process.env.FLAGSMITH_KEY as string,
-	requestTimeout: 60
+  environmentKey: process.env.FLAGSMITH_KEY as string,
+  requestTimeout: 60,
 });
 
 export default flagsmith;
