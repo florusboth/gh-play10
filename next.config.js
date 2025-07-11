b22101c3e7a767e4911732567575aef65dce1c77
florus <florus@tackyon.it>
2025-07-11 12:49:32 +0200

    remove appDir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
   },
   // Enable standalone output for Docker deployment
   output: "standalone",
