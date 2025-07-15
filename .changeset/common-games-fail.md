924f67e16fba5107284c048ab0e8ef31bc11eb52
florus <florus@tackyon.it>
2025-07-15 13:26:02 +0200

    add text + changeset

---
+++ .changeset/common-games-fail.md
@@ -0,0 +1,5 @@
+---
+"next-gha-play": patch
+---
+
+add text
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
+        <p>New Text!</p>
       </div>
       <Link href="/about">About</Link>
       <br />
