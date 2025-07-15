87758bb7ef800c75e492aef99b8ddf7867a70277
florusboth <florus@yeetta.com>
2025-07-15 15:23:33 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
-        <p>New Text!</p>
       </div>
       <Link href="/about">About</Link>
       <br />
